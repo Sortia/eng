78b005ea8aedde69e612aca69ee338e4b342c6c7
Sortia <alex-kiyan.lug@mail.ru>
2019-07-20 02:54:51 +0300

    change table names (DB)

--- app/Models/Block.php
+++ app/Models/Block.php
@@ -4,22 +4,24 @@ namespace App\Models;
 
 class Block extends Model
 {
+    static public $table = 'blocks';
+
     static public function create($block_name)
     {
-        self::$link->query("INSERT INTO block (name, status) VALUES ('$block_name', 0);");
+        self::$link->query("INSERT INTO " . self::$table . " (name, status) VALUES ('$block_name', 0);");
 
-        return self::$link->query("SELECT * FROM block WHERE id = (" . self::$link->lastInsertId() . ")")->fetch();
+        return self::$link->query("SELECT * FROM " . self::$table . " WHERE id = (" . self::$link->lastInsertId() . ")")->fetch();
     }
 
     static public function read()
     {
-        return self::$link->query("SELECT * FROM block ORDER BY id desc;")->fetchAll();
+        return self::$link->query("SELECT * FROM " . self::$table . " ORDER BY id desc;")->fetchAll();
     }
 
     static public function update($status, $block_id)
     {
-         self::$link->query("UPDATE item SET status = '$status' WHERE block_id = '$block_id';");
-         self::$link->query("UPDATE block SET status = '$status' WHERE id = '$block_id';");
+         self::$link->query("UPDATE " . Item::$table . " SET status = '$status' WHERE block_id = '$block_id';");
+         self::$link->query("UPDATE " . self::$table . " SET status = '$status' WHERE id = '$block_id';");
 
         return self::$link->query("SELECT * FROM block WHERE id = '$block_id'")->fetch();
     }
@@ -28,11 +30,11 @@ class Block extends Model
     {
         self::$link->beginTransaction();
         self::$link->query("
-            DELETE FROM item WHERE block_id = '$block_id';
-            DELETE FROM block WHERE id = '$block_id';
+            DELETE FROM " . Item::$table . " WHERE block_id = '$block_id';
+            DELETE FROM " . self::$table . " WHERE id = '$block_id';
         ");
         self::$link->commit();
 
-        return (bool)!self::$link->query("SELECT EXISTS(SELECT 1 FROM item WHERE id ='$block_id' LIMIT 1)")->fetchColumn();
+        return (bool)!self::$link->query("SELECT EXISTS(SELECT 1 FROM " . self::$table . " WHERE id ='$block_id' LIMIT 1)")->fetchColumn();
     }
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,30 +4,32 @@ namespace App\Models;
 
 class Item extends Model
 {
+    static public $table = 'items';
+
     static public function create($rus, $eng, $block_id)
     {
-        self::$link->query("INSERT INTO item (rus, eng, block_id) VALUES ('$rus', '$eng', '$block_id');");
+        self::$link->query("INSERT INTO " . self::$table . " (rus, eng, block_id) VALUES ('$rus', '$eng', '$block_id');");
 
-        return self::$link->query("SELECT * FROM item WHERE id = (" . self::$link->lastInsertId() . ")")->fetch();
+        return self::$link->query("SELECT * FROM " . self::$table . " WHERE id = (" . self::$link->lastInsertId() . ")")->fetch();
     }
 
     static public function read($block_id)
     {
-        return self::$link->query("SELECT * FROM item WHERE block_id = $block_id ORDER BY id desc;")->fetchAll();
+        return self::$link->query("SELECT * FROM " . self::$table . " WHERE block_id = $block_id ORDER BY id desc;")->fetchAll();
     }
 
     static public function update($status, $item_id)
     {
-        self::$link->query("UPDATE item SET status = '$status' WHERE id = '$item_id';");
+        self::$link->query("UPDATE " . self::$table . " SET status = '$status' WHERE id = '$item_id';");
 
-        return self::$link->query("SELECT * FROM item WHERE id = '$item_id'")->fetch();
+        return self::$link->query("SELECT * FROM " . self::$table . " WHERE id = '$item_id'")->fetch();
     }
 
     static public function delete($item_id)
     {
-        self::$link->query("DELETE FROM item WHERE id = '$item_id';");
+        self::$link->query("DELETE FROM " . self::$table . " WHERE id = '$item_id';");
 
-        return (bool)!self::$link->query("SELECT EXISTS(SELECT 1 FROM item WHERE id ='$item_id' LIMIT 1)")->fetchColumn();
+        return (bool)!self::$link->query("SELECT EXISTS(SELECT 1 FROM " . self::$table . " WHERE id ='$item_id' LIMIT 1)")->fetchColumn();
 
     }
 }
