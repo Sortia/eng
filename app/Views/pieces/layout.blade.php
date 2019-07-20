9c85fc69da0cd390240bf311042a3b69ebe03371
Sortia <alex-kiyan.lug@mail.ru>
2019-07-20 18:44:34 +0300

    bootstrap, fix conflict

--- app/Views/pieces/layout.blade.php
+++ app/Views/pieces/layout.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <title>ToLearn</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <link rel="stylesheet" href="public/css/icons.css">
     <link rel="stylesheet" href="public/css/index.css">
     <link rel="stylesheet" href="public/fonts/fontello-e129e090/css/fontello.css">
@@ -14,6 +16,9 @@
     <script src="public/js/ajax.js"></script>
     <script src="public/js/app.js"></script>
     <script src="public/js/events.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
