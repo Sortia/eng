a76e35b60e1c31a3ecae28b63a493d3ce7f182f4
alex <alexkiyan.lug@gmail.com>
2019-07-27 19:42:40 +0300

    update DB

--- eng.sql
+++ eng.sql
@@ -1,82 +1,130 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Июл 27 2019 г., 18:41
+-- Версия сервера: 10.3.16-MariaDB
+-- Версия PHP: 7.3.7
 
- Source Server         : MySQL
- Source Server Type    : MySQL
- Source Server Version : 80012
- Source Host           : localhost:3306
- Source Schema         : eng
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 80012
- File Encoding         : 65001
 
- Date: 20/01/2019 20:18:00
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- База данных: `eng`
+--
 
--- ----------------------------
--- Table structure for block
--- ----------------------------
-DROP TABLE IF EXISTS `block`;
-CREATE TABLE `block`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `blocks`
+--
+
+CREATE TABLE `blocks` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `status` tinyint(4) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `name` varchar(255) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Records of block
--- ----------------------------
-INSERT INTO `block` VALUES (1, 'first_block', 0);
-INSERT INTO `block` VALUES (2, 'second_block', 0);
-INSERT INTO `block` VALUES (3, 'third_block', 0);
+--
+-- Триггеры `blocks`
+--
+DELIMITER $$
+CREATE TRIGGER `delete_items` BEFORE DELETE ON `blocks` FOR EACH ROW DELETE FROM items WHERE block_id = OLD.id
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_items` AFTER UPDATE ON `blocks` FOR EACH ROW UPDATE items SET status = NEW.status WHERE block_id = NEW.id
+$$
+DELIMITER ;
 
--- ----------------------------
--- Table structure for item
--- ----------------------------
-DROP TABLE IF EXISTS `item`;
-CREATE TABLE `item`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `rus` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `eng` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `status` tinyint(4) NULL DEFAULT NULL,
-  `block_id` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 272 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Records of item
--- ----------------------------
-INSERT INTO `item` VALUES (171, 'много', 'much', 1, 1);
-INSERT INTO `item` VALUES (172, 'поощрять / воодушевлять', 'encourage', 1, 1);
-INSERT INTO `item` VALUES (173, 'порядочный', 'decent', 1, 1);
-INSERT INTO `item` VALUES (174, 'живая природа', 'wildlife', 0, 1);
-INSERT INTO `item` VALUES (175, 'в целом', 'overall', 0, 1);
-INSERT INTO `item` VALUES (176, 'что-то / нечто', 'something', 1, 2);
-INSERT INTO `item` VALUES (177, 'упомянутый', 'mentioned', 1, 2);
-INSERT INTO `item` VALUES (178, 'окруженный', 'surrounded', 0, 2);
-INSERT INTO `item` VALUES (179, 'определенный', 'certain', 0, 2);
-INSERT INTO `item` VALUES (180, 'кто-нибудь', 'anyone', 0, 2);
-INSERT INTO `item` VALUES (181, 'иногда / время от времени', 'occasionally', 1, 2);
-INSERT INTO `item` VALUES (182, 'очевидно', 'obviosly', 1, 2);
-INSERT INTO `item` VALUES (183, 'график', 'schedule', 0, 2);
-INSERT INTO `item` VALUES (185, 'как угодно', 'like anything', 0, 2);
-INSERT INTO `item` VALUES (186, 'должен попробовать', 'should try out', 0, 3);
-INSERT INTO `item` VALUES (187, 'возможность', 'opportinutie', 0, 3);
-INSERT INTO `item` VALUES (188, 'свежий', 'fiest', 0, 3);
-INSERT INTO `item` VALUES (189, 'вкусные', 'delecious', 0, 3);
-INSERT INTO `item` VALUES (190, 'прямо', 'straight', 0, 3);
-INSERT INTO `item` VALUES (191, 'особенно', 'especially', 0, 3);
-INSERT INTO `item` VALUES (192, 'узнать', 'learn', 0, 3);
-INSERT INTO `item` VALUES (193, 'независимо от того', 'no metter', 0, 3);
-INSERT INTO `item` VALUES (194, 'приблизительно', 'roughly', 0, 3);
-INSERT INTO `item` VALUES (195, 'обычно / вообщем-то', 'generaly', 0, 3);
-INSERT INTO `item` VALUES (196, 'письменный стол', 'desk', 0, 3);
-INSERT INTO `item` VALUES (197, 'кто-нибудь / любой', 'anyone', 0, 3);
-INSERT INTO `item` VALUES (198, 'имеет значение', 'metter', 0, 3);
+--
+-- Структура таблицы `items`
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `rus` varchar(255) DEFAULT NULL,
+  `eng` varchar(255) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  `block_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `email`) VALUES
+(1, 'Sortia', '$2y$10$0uQj.N/dF7lH.AM/S6FGWORo1fkJQXLNpFVnoEvwXwBct/RKbyA5C', 'alex-kiyan.lug@mail.ru');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `blocks`
+--
+ALTER TABLE `blocks`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Индексы таблицы `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `blocks`
+--
+ALTER TABLE `blocks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT для таблицы `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=279;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
