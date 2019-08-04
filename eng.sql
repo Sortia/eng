5e90e71c54eac9f65b9b36a01b0fb087f1554230
alex <alexkiyan.lug@gmail.com>
2019-08-04 20:32:05 +0300

    update db

--- eng.sql
+++ eng.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost
--- Время создания: Июл 27 2019 г., 18:41
+-- Время создания: Авг 04 2019 г., 19:29
 -- Версия сервера: 10.3.16-MariaDB
 -- Версия PHP: 7.3.7
 
@@ -25,24 +25,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `blocks`
+-- Структура таблицы `flashcards`
 --
 
-CREATE TABLE `blocks` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `status` tinyint(4) DEFAULT NULL
+CREATE TABLE `flashcards` (
+                              `id` int(11) NOT NULL,
+                              `name` varchar(255) DEFAULT NULL,
+                              `status` tinyint(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 --
--- Триггеры `blocks`
+-- Дамп данных таблицы `flashcards`
+--
+
+INSERT INTO `flashcards` (`id`, `name`, `status`) VALUES
+(16, '1235', 0),
+(17, '1234', 1);
+
+--
+-- Триггеры `flashcards`
 --
 DELIMITER $$
-CREATE TRIGGER `delete_items` BEFORE DELETE ON `blocks` FOR EACH ROW DELETE FROM items WHERE block_id = OLD.id
+CREATE TRIGGER `delete_items` BEFORE DELETE ON `flashcards` FOR EACH ROW DELETE FROM items WHERE flashcard_id = OLD.id
 $$
 DELIMITER ;
 DELIMITER $$
-CREATE TRIGGER `update_items` AFTER UPDATE ON `blocks` FOR EACH ROW UPDATE items SET status = NEW.status WHERE block_id = NEW.id
+CREATE TRIGGER `update_items` AFTER UPDATE ON `flashcards` FOR EACH ROW UPDATE items SET status = NEW.status WHERE flashcard_id = NEW.id
 $$
 DELIMITER ;
 
@@ -53,12 +61,24 @@ DELIMITER ;
 --
 
 CREATE TABLE `items` (
-  `id` int(11) NOT NULL,
-  `rus` varchar(255) DEFAULT NULL,
-  `eng` varchar(255) DEFAULT NULL,
-  `status` tinyint(4) DEFAULT NULL,
-  `block_id` int(11) DEFAULT NULL
+                         `id` int(11) NOT NULL,
+                         `rus` varchar(255) DEFAULT NULL,
+                         `eng` varchar(255) DEFAULT NULL,
+                         `status` tinyint(4) DEFAULT NULL,
+                         `flashcard_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+--
+-- Дамп данных таблицы `items`
+--
+
+INSERT INTO `items` (`id`, `rus`, `eng`, `status`, `flashcard_id`) VALUES
+(279, 'ÐžÐ´Ð¸Ð½', '1', 0, 16),
+(280, 'Ð”Ð²Ð°', '2', 0, 16),
+(281, 'Ð¢Ñ€Ð¸', '3', 0, 16),
+(282, 'ÐŸÑÑ‚ÑŒ', '5', 0, 16),
+(283, '432', '1123', 1, 17),
+(284, '312', '123', 1, 17);
 
 -- --------------------------------------------------------
 
@@ -67,10 +87,10 @@ CREATE TABLE `items` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `login` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+                         `id` int(11) NOT NULL,
+                         `login` varchar(255) NOT NULL,
+                         `password` varchar(255) NOT NULL,
+                         `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -78,51 +98,52 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `email`) VALUES
-(1, 'Sortia', '$2y$10$0uQj.N/dF7lH.AM/S6FGWORo1fkJQXLNpFVnoEvwXwBct/RKbyA5C', 'alex-kiyan.lug@mail.ru');
+(1, 'Sortia', '$2y$10$0uQj.N/dF7lH.AM/S6FGWORo1fkJQXLNpFVnoEvwXwBct/RKbyA5C', 'alex-kiyan.lug@mail.ru'),
+(2, 'Sortia', '$2y$10$lqZQh.9DUzKyW7b2S0A64.shkH3n/yoyESKts4vsTR0DuYOjvcdOe', 'alex-kiyan.lug@mail.ru');
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
--- Индексы таблицы `blocks`
+-- Индексы таблицы `flashcards`
 --
-ALTER TABLE `blocks`
-  ADD PRIMARY KEY (`id`) USING BTREE;
+ALTER TABLE `flashcards`
+    ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Индексы таблицы `items`
 --
 ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`) USING BTREE;
+    ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT для таблицы `blocks`
+-- AUTO_INCREMENT для таблицы `flashcards`
 --
-ALTER TABLE `blocks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+ALTER TABLE `flashcards`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT для таблицы `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=279;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=285;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
