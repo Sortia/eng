1ce72010ab3dbdea7532547b521eccb2d74a9d43
alex <alexkiyan.lug@gmail.com>
2019-08-07 21:29:45 +0300

    update helpers

--- app/helpers.php
+++ app/helpers.php
@@ -3,47 +3,38 @@
 use Jenssegers\Blade\Blade;
 
 /**
- * Created by PhpStorm.
- * User: Яяя
- * Date: 19.01.2019
- * Time: 13:32
- * @param string $var1
- * @param string $var2
- * @param string $var3
+ * dump variable and die
+ *
+ * @param mixed ...$vars
  */
-
-function dd($var1 = "not_found", $var2 = "not_found", $var3 = "not_found")
+function dd(...$vars)
 {
     echo '<pre>';
-    if ($var1 !== "not_found") print_r($var1);
-    echo "<br>";
-    if ($var2 !== "not_found") print_r($var2);
-    echo "<br>";
-    if ($var3 !== "not_found") print_r($var3);
-    echo "<br>";
-    echo '<pre>';
+
+    foreach ($vars as $var)
+        print_r($var);
 
     exit;
 }
 
-function d($var1 = "not_found", $var2 = "not_found", $var3 = "not_found")
+/**
+ * dump variable
+ *
+ * @param mixed ...$vars
+ */
+function d(...$vars)
 {
     echo '<pre>';
-    if ($var1 !== "not_found") var_dump($var1);
-    echo "<br>";
-    if ($var2 !== "not_found") var_dump($var2);
-    echo "<br>";
-    if ($var3 !== "not_found") print_r($var3);
-    echo "<br>";
-    echo '<pre>';
+
+    foreach ($vars as $var)
+        print_r($var);
 }
 
-function asset($path) : string
-{
-    return ROOT . '/' . $path;
-}
-
-function response($data = true, $status = 200)
+/**
+ * @param array $data
+ * @param int $status
+ */
+function response($data = [], $status = 200)
 {
     if ($status !== 200) {
         header('HTTP/1.1 500 Internal Server Booboo');
@@ -61,20 +52,10 @@ function view($template, $data = [])
     echo $blade->make($template, $data);
 }
 
+/**
+ * @return bool
+ */
 function auth() : bool
 {
     return \App\Models\Auth::isAuth();
-}
-
-function array_unset_val(&$array, $key)
-{
-    if (isset($array[$key])) {
-        $val = $array[$key];
-
-        unset($array[$key]);
-
-        return $val;
-    } else {
-        return null;
-    }
 }
